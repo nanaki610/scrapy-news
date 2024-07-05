9501209fffd771f72fae2e156ac94c0ed3ff0732
kawano <nanaki610@gmail.com>
2024-07-06 02:24:49 +0900

    refactor: Close Playwright page after parsing in Yahoo News spider

--- yahoo/spiders/news.py
+++ yahoo/spiders/news.py
@@ -44,45 +44,56 @@ class NewsSpider(scrapy.Spider):
             
     async def parse(self, response):
         page = response.meta['playwright_page']
-        # await page.close() # playwrigthのページを閉じる
+        await page.close() # playwrigthのページを閉じる
         
         today = self.get_today() # 今日の日付を取得
         
         articles = response.css('li.newsFeed_item') # scrapyのセレクタで記事を取得
-        for article in articles:
+        for (index, article) in enumerate(articles):
             title = article.css('div.newsFeed_item_title::text').get() # タイトルを取得
             postDate = self.convert_date(article.css('time.newsFeed_item_date::text').get()) # 投稿日を取得
             url = article.css('a.newsFeed_item_link::attr(href)').get() # URLを取得
             
             #取得した投稿日が今日ではない場合は処理を終了
-            if postDate['date'] != today:
-                break
+            # if postDate['date'] != today:
+            #     break
             
             #記事のリンクに移動
+            # article = scrapy.Request(
             yield scrapy.Request(
                 url,
                 meta={
                     'playwright': True,
                     'playwright_include_page': True,
                     'playwright_page_methods': [
-                        PageMethod('wait_for_selector', 'div#uamods-topics'),
-                        PageMethod('click', get_by_text='記事全文を読む'),
-                        PageMethod('screenshot', path="articledetail.png", full_page=True),
+                        PageMethod('wait_for_selector', 'article#uamods-pickup'),
+                        # PageMethod('click', '//a[contains(text(), "記事全文を読む")]', timeout=5000, wait_for='load'),
+                        PageMethod('click', 'a.bxbqJP'),
+                        PageMethod('screenshot', path=f"articledetail{index}.png", full_page=True),
                     ],
                     "callback": self.parse_article,
                     "errback": self.errback,
                 }
             )
-            break
+
             # 取得したデータをyieldで返す
             yield {
                 'title': title,
                 'postDate': f"{postDate['date']}{postDate['time']}",
                 'url': url,
+                'article': article,
             }
 
     async def parse_article(self, response):
-        pass
+        page = response.meta['playwright_page']
+        await page.close()
+        
+        #記事の内容を取得
+        content = await page.content()
+        selector = Selector(text=content)
+        article = selector.css('article#uamods').get()
+        return article
+        
 
     async def errback(self, failure):
         page = failure.request.meta["playwright_page"]
