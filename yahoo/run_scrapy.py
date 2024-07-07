1e3ec1c3e24e8ed3d1cae824e5b92f385b8be8c9
kawano <nanaki610@gmail.com>
2024-07-08 02:20:30 +0900

    chore: Update Yahoo News spider to include start and end time logging

--- yahoo/run_scrapy.py
+++ yahoo/run_scrapy.py
@@ -15,12 +15,18 @@ from scrapy.utils.project import get_project_settings
 from spiders.news import NewsSpider
 from common_func import  post_slack
 
+#現在時刻(JST)をYYYYMMDDhhmm形式で取得
+tokyo_timezone = pytz.timezone('Asia/Tokyo')
+start_time = datetime.now(tokyo_timezone).strftime('%Y/%m/%d %H:%M')
+print(f"スクレイピング開始時刻: {start_time}")
+
 # Yahooニュースのスパイダーを実行
 process = CrawlerProcess(settings=get_project_settings())
 process.crawl(NewsSpider)
 process.start()
 
-#現在時刻(JST)をYYYYMMDDhhmm形式で取得
-tokyo_timezone = pytz.timezone('Asia/Tokyo')
-now = datetime.now(tokyo_timezone).strftime('%Y%m%d%H%M')
-post_slack(f"Yahoo Newsのスクレイピングが完了しました：{now}")
+end_time = datetime.now(tokyo_timezone).strftime('%Y/%m/%d %H:%M')
+exect_time = (datetime.now() - datetime.strptime(start_time, '%Y/%m/%d %H:%M')).seconds / 60 # 経過時間(分)
+
+print(f"スクレイピング終了時刻: {end_time}")
+post_slack(f"Yahoo Newsのスクレイピングが完了しました。実行時間:{exect_time}分")
